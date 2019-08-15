3387d9db58be17d2f4d29857089ca14c36b0e040
romkravets <romann.kravets@gmail.com>
2019-08-15 11:19:28 +0300

    Added change

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,13 @@
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
 
  class Header extends React.Component {
     constructor() {
@@ -11,39 +18,59 @@ import React from "react"
     }
   }
     render() {
+      function toggleMenu() {
+        if (!showMenu) {
+          menuBtn.classList.add('close');
+          menu.classNameList.add('show');
+          menuNav.classList.add('show');
+          menuBranding.classList.add('show');
+          navItems.forEach(item => item.classList.add('show'));
+      
+          // Set Menu State
+          this.showMenu = true;
+        } else {
+          menuBtn.classList.remove('close');
+          menu.classList.remove('show');
+          menuNav.classList.remove('show');
+          menuBranding.classList.remove('show');
+          navItems.forEach(item => item.classList.remove('show'));
+      
+          // Set Menu State
+          this.showMenu = false;
+        }
+      }
         return(
         <header>
-          <div class="menu-btn">
-            <div class="btn-line"></div>
-            <div class="btn-line"></div>
-            <div class="btn-line"></div>
+          <div className="menu-btn"onClick={toggleMenu}>
+            <div className="btn-line"></div>
+            <div className="btn-line"></div>
+            <div className="btn-line"></div>
           </div>
           <p>Count: {this.state.count}</p>
             <button onClick={() => this.setState({count: this.state.count + 1})}>+</button>
             <button onClick={() => this.setState({count: this.state.count - 1})}>-</button>
-
-          <nav class="menu">
-            <div class="menu-branding">
-              <div class="portrait"></div>
+          <nav className="menu">
+            <div className="menu-branding">
+              <div className="portrait"></div>
             </div>
-            <ul class="menu-nav">
-              <li class="nav-item current">
-                <a href="index.html" class="nav-link">
+            <ul className="menu-nav">
+              <li className="nav-item current">
+                <a href="index.html" className="nav-link">
                   Home
                 </a>
               </li>
-              <li class="nav-item">
-                <a href="about.html" class="nav-link">
+              <li className="nav-item">
+                <a href="about.html" className="nav-link">
                   About Me
                 </a>
               </li>
-              <li class="nav-item">
-                <a href="work.html" class="nav-link">
+              <li className="nav-item">
+                <a href="work.html" className="nav-link">
                   My Work
                 </a>
               </li>
-              <li class="nav-item">
-                <a href="contact.html" class="nav-link">
+              <li className="nav-item">
+                <a href="contact.html" className="nav-link">
                   How To Reach Me
                 </a>
               </li>
