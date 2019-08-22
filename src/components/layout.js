050c7e2d8e2ce428edc5b5f6e3f145abbf37d313
romkravets <romann.kravets@gmail.com>
2019-08-22 22:13:14 +0300

    delete menu old

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,6 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import Menu from "./menu"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -27,7 +26,6 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      {/* <Menu/> */}
       <div
         style={{
           margin: `0 auto`,
