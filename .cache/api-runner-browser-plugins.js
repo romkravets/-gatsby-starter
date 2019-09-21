357a1b80b6317b04436da69078e1a4b51205e957
romkravets <romann.kravets@gmail.com>
2019-09-21 10:57:08 +0300

    Updates

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('C:/Users/user_1/Documents/GitHub/-gatsby-starter/node_modules/gatsby-plugin-layout/gatsby-browser.js'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('C:/Users/user_1/Documents/GitHub/-gatsby-starter/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
